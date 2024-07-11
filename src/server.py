83950b0afcb4de5b8149dd0f8a9892ab80d6833f
Faye Billye <fbillye@student.21-school.ru>
2024-07-11 19:27:25 +0700

    Update server.py

--- src/server.py
+++ src/server.py
@@ -147,7 +147,8 @@ def hello_world():
     return 'Easy Server'
 
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
